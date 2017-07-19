81860ee6ec162da0488bdaf2aba74412b1a4517b
Jorge <jorgefdt@gmail.com>
2017-07-19 12:42:18 -0400

    Customized Vertx and Netty logging to use Log4J2.

--- src/main/java/com/viafoura/palindromes/AppLauncher.java
+++ src/main/java/com/viafoura/palindromes/AppLauncher.java
@@ -1,5 +1,8 @@
 package com.viafoura.palindromes;
 
+import io.netty.util.internal.logging.InternalLoggerFactory;
+import io.netty.util.internal.logging.Log4J2LoggerFactory;
+
 import java.io.IOException;
 
 /**
@@ -17,6 +20,15 @@ public class AppLauncher {
     }
 
     private static void startServer(final String fileName) {
+        customizeVertxLogging();
         new ServerLauncher().launchServer(fileName);
+    }
+
+    private static void customizeVertxLogging() {
+        // Force Vertx logging to Log4J2.
+        System.setProperty("vertx.logger-delegate-factory-class-name", "io.vertx.core.logging.Log4j2LogDelegateFactory");
+
+        // Force Netty logging to Log4j.
+        InternalLoggerFactory.setDefaultFactory(Log4J2LoggerFactory.INSTANCE);
     }
 }
